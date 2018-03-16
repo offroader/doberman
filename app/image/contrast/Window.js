222065624c723da6c1325ac3cc0eb6ee0f89f87d
giorgi <giorgishvelidze@gmail.com>
2018-03-17 03:31:33 +0400

    improved contrast window

--- app/image/contrast/Window.js
+++ app/image/contrast/Window.js
@@ -13,22 +13,20 @@ Ext.define('app.image.contrast.Window', {
         var that = this
         
         var slider = Ext.create('Ext.slider.Single', {
-    	    minValue: -128,
-    	    maxValue: 128,
+            minValue: -128,
+            maxValue: 128,
             value: config.currentValue,
-    	    increment: 1,
+            increment: 1,
             hideLabel: true,
             anchor: '100%',
             listeners: {
-            	change: function (f, value) {
-            		that.fireEvent('contrastChange', value)
-            	}
+                change: function (f, value) {
+                    that.fireEvent('contrastChange', value)
+                }
             }
         })
         
         that.items = [slider]
-        
-        window.slider = slider
         
         that.buttons = [{
             text: 'შენახვა',
@@ -45,8 +43,5 @@ Ext.define('app.image.contrast.Window', {
         }]
         
         that.callParent(arguments)
-        
-        
-                
     }
 }) 
